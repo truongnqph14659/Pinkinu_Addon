80dd91b0d91ad198d5613966cc18ea7419a9c4f3
truongnqph14659 <truongrubi717@gmail.com>
2023-04-04 17:24:00 +0700

    buy now design

--- src/layout/DashboardLayout/index.tsx
+++ src/layout/DashboardLayout/index.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
-    <div className="flex h-[100vh] items-center justify-center bg-[#f0749b]">
+    <div className="flex h-[100vh] items-center justify-center bg-white">
       <Outlet />
     </div>
   );
